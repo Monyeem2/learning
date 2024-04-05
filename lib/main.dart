936eb8f417e4ff1695ccc7fd47700c935853a39d
Monyeem2 <mamonyeem@gmail.com>
2024-04-05 15:04:33 +0600

    Project name change

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Demo BMI',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
